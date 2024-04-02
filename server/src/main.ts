48066a38f5f2631caa2fbdb2994fdf26bff480bb
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-04-02 10:57:29 +0330

    add main server

---
+++ server/src/main.ts
@@ -0,0 +1,25 @@
+﻿import { NestFactory } from '@nestjs/core';
+import { callBackListener } from './modules/utils/callBack.main';
+import { SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ConfigService } from '@nestjs/config';
+import { configSwagger } from 'src/modules/config/swagger.config';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    logger: ['error', 'warn', 'debug'],
+    
+  });
+  const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
+  const port = configService.get('App.port');
+  app.useStaticAssets(join(process.cwd(), '../public'));
+  const document = SwaggerModule.createDocument(app, configSwagger);
+  SwaggerModule.setup('api/swagger', app, document);
+  await app.listen(port, () => callBackListener(port));
+}
+bootstrap();
