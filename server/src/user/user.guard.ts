a8aeadb840ce80fc66cf6a6aa4a4dfacc24b9bf3
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-04-04 02:02:52 +0330

    Create user.guard.ts

---
+++ server/src/user/user.guard.ts
@@ -0,0 +1,31 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserLicensePlatesEntity } from 'src/modules/entities/mysql/user-license-plates.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ExiLicensePlatesGuard implements CanActivate {
+  constructor(
+    @InjectRepository(UserLicensePlatesEntity)
+    private readonly userLicensePlatesEntityRepository: Repository<UserLicensePlatesEntity>,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const { uuid } = request.body;
+    if (!uuid)
+      throw new HttpException('uuid وجود ندارد', HttpStatus.NOT_FOUND);
+    const licensePlate = await this.userLicensePlatesEntityRepository.find({
+      where: { user_license_plates_id: uuid },
+    });
+    if (!licensePlate.length)
+      throw new HttpException('uuid وجود ندارد', HttpStatus.NOT_FOUND);
+    return true;
+  }
+}
