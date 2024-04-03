f21c0b9b3cc5ce30f6f6193bf2be2881a006c620
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-04-04 02:01:19 +0330

    Update auth.module.ts

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -6,13 +6,26 @@ import { OtpEntity } from 'src/modules/entities/mysql/otp.entity';
 import { TokenUserTelegramEntity } from 'src/modules/entities/mysql/token-user-telegram.entity';
 import { UserEntity } from 'src/modules/entities/mysql/user.entity';
 import { JwtModule } from 'src/modules/jwt/jwt.module';
+import { UserRoleEntity } from 'src/modules/entities/mysql/user-role.entity';
+import { RoleEntity } from 'src/modules/entities/mysql/role.entity';
+import { VerifyTokenMiddleware } from 'src/modules/common/middleware/verify-token.middleware';
 
 @Module({
   imports: [
     JwtModule,
-    TypeOrmModule.forFeature([OtpEntity, TokenUserTelegramEntity, UserEntity]),
+    TypeOrmModule.forFeature([
+      UserRoleEntity,
+      RoleEntity,
+      OtpEntity,
+      TokenUserTelegramEntity,
+      UserEntity,
+    ]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
 })
-export class AuthModule {}
+export class AuthModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(VerifyTokenMiddleware).forRoutes('/admin/*');
+  }
+}
