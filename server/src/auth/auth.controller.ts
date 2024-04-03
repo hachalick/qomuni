2786f5371c35f530a55e5d434cc50c9faaa5211d
Hossein jalali <149144798+hachalick@users.noreply.github.com>
2024-04-04 02:01:17 +0330

    Update auth.controller.ts

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Put, UseGuards } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post, Put, UseGuards } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import {
@@ -23,12 +23,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('send-otp-sms')
+  @HttpCode(HttpStatus.CREATED)
   @UseGuards(SendOtpSmsGuard)
   sendOtpSms(@Body() body: SendOtpSmsDto) {
     return this.authService.sendOtpSms(body);
   }
-
+  
   @Post('sign-up')
+  @HttpCode(HttpStatus.CREATED)
   @UseGuards(CheckOtpGuard)
   @UseGuards(NotExistUserWithMobileGuard)
   signUp(@Body() body: SignUpDto) {
@@ -36,18 +38,21 @@ export class AuthController {
   }
 
   @Post('sign-in')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(CheckOtpGuard)
   signIn(@Body() body: SignInDto) {
     return this.authService.signIn(body);
   }
 
   @Put('refresh-token')
+  @HttpCode(HttpStatus.CREATED)
   @UseGuards(ExpRefreshTokenGuard)
   refreshToken(@Body() body: RefreshTokenDto) {
     return this.authService.refreshToken(body.refresh_token);
   }
 
   @Put('forgot-password')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(CheckOtpGuard)
   @UseGuards(ExistUserWithMobileGuard)
   forgotPassword(@Body() body: ForgotPasswordDto) {
@@ -55,6 +60,7 @@ export class AuthController {
   }
 
   @Put('forgot-token-tel')
+  @HttpCode(HttpStatus.OK)
   @UseGuards(CheckOtpGuard)
   @UseGuards(ExistUserWithMobileGuard)
   forgotTokenTel(@Body() body: ForgotTokenTelDto) {
